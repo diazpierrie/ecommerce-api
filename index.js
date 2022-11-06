78bb5188548a6e95fb41b50a99233e3da52cbf7c
Ezequiel <ezequieldiazpierri@gmail.com>
2022-11-06 02:59:20 -0300

    Models, some routes, password encrypt

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connection successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.get("/api/test", () => {
+  console.log("memeking");
+});
+
+app.listen(process.env.PORT || 5000, () => {});
